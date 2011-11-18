0c95477ccede4ea26477b754f94b010ee405c0d2
amphiprions@gmail.com <amphiprions@gmail.com@5c5feef0-fd83-750a-74bc-0a59a23baf9e>
2011-11-18 10:49:13 +0000

    Add tiles and update DB

--- trunk/MansionOfMadnessToolKit/src/org/amphiprion/mansionofmadness/database/DatabaseHelper.java
+++ trunk/MansionOfMadnessToolKit/src/org/amphiprion/mansionofmadness/database/DatabaseHelper.java
@@ -20,6 +20,7 @@
 package org.amphiprion.mansionofmadness.database;
 
 import org.amphiprion.mansionofmadness.ApplicationConstants;
+import org.amphiprion.mansionofmadness.dto.Tile;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
@@ -40,10 +41,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		try {
-			// db.execSQL("create table GAME (" + Game.DbField.ID +
-			// " text primary key, " + Game.DbField.NAME + " text not null, " +
-			// Game.DbField.IMAGE_NAME + " text" + ","
-			// + Game.DbField.PLAYER_SUMMARY + " text" + ") ");
+			db.execSQL("create table TILE (" + Tile.DbField.ID + " text primary key, " + Tile.DbField.NAME + " text not null, " + Tile.DbField.IMAGE_NAME + " text" + ","
+					+ Tile.DbField.IS_EMBEDDED + " number(1)" + ") ");
+
+			db.execSQL("insert into TILE values('1','attic','attic.png',1) ");
+			db.execSQL("insert into TILE values('2','corridor_angle','corridor_angle.png',1) ");
+			db.execSQL("insert into TILE values('3','diner_room','diner_room.png',1) ");
+			db.execSQL("insert into TILE values('4','driveway','driveway.png',1) ");
+			db.execSQL("insert into TILE values('5','garden','garden.png',1) ");
+			db.execSQL("insert into TILE values('6','guest_room','guest_room.png',1) ");
+			db.execSQL("insert into TILE values('7','loby','loby.png',1) ");
+			db.execSQL("insert into TILE values('8','park','park.png',1) ");
+			db.execSQL("insert into TILE values('9','salon','salon.png',1) ");
+			db.execSQL("insert into TILE values('10','study','study.png',1) ");
+			db.execSQL("insert into TILE values('11','tower','tower.png',1) ");
 
 			onUpgrade(db, 1, DATABASE_VERSION);
 		} catch (Throwable e) {
