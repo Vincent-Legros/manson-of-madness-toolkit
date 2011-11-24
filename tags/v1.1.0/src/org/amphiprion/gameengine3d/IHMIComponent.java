a37d0f08b3cdf5fd7746050351c6bb263c7ca61f
amphiprions@gmail.com <amphiprions@gmail.com@5c5feef0-fd83-750a-74bc-0a59a23baf9e>
2011-11-24 11:39:53 +0000

    useless

--- tags/v1.1.0/src/org/amphiprion/gameengine3d/IHMIComponent.java
+++
@@ -1,9 +0,0 @@
-package org.amphiprion.gameengine3d;
-
-public interface IHMIComponent extends IObject2D {
-	void setEnable(boolean enable);
-
-	boolean isEnable();
-
-	boolean contains(int px, int py);
-}
